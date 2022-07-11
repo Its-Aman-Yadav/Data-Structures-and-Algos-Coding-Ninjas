daf5082bf00f36b3915c3b9efdce129b2daf3d5a
Mr Aman yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2022-07-11 21:13:05 +0530

    Update Stack using LL.cpp

--- Stacks/Stack using LL.cpp
+++ Stacks/Stack using LL.cpp
@@ -92,3 +92,99 @@ The fourth one is Query-5: It tells whether the stack is empty at this point or 
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
   
+#include <iostream>
+using namespace std;
+
+class Node {
+   public:
+    int data;
+    Node *next;
+
+    Node(int data) {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+class Stack
+{
+    // Define the data members
+    Node *head;
+    int size;
+
+public:
+    Stack()
+    {
+        head = NULL;
+        size = 0;
+    }
+
+    int getSize()
+    {
+        return size;
+    }
+
+    bool isEmpty()
+    {
+        return head == NULL;
+    }
+
+    void push(int element)
+    {
+        Node *temp = new Node(element);
+        temp->next = head;
+        head = temp;
+        size++;
+    }
+
+    int pop()
+    {
+        if (isEmpty())
+            return -1;
+        Node *temp = NULL;
+        int num = head->data;
+        temp = head->next;
+        delete head;
+        head = temp;
+        size--;
+        return num;
+    }
+
+    int top()
+    {
+        if (isEmpty())
+            return -1;
+        return head->data;
+    }
+};
+
+
+int main() {
+    Stack st;
+
+    int q;
+    cin >> q;
+
+    while (q--) {
+        int choice, input;
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                cin >> input;
+                st.push(input);
+                break;
+            case 2:
+                cout << st.pop() << "\n";
+                break;
+            case 3:
+                cout << st.top() << "\n";
+                break;
+            case 4:
+                cout << st.getSize() << "\n";
+                break;
+            default:
+                cout << ((st.isEmpty()) ? "true\n" : "false\n");
+                break;
+        }
+    }
+}
